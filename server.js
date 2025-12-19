d63090377780c014cbd8040ddc784c433d327910
Kishore <kishore.vr2024aiml@sece.ac.in>
2025-12-19 10:10:39 +0530

    Add CORS support for Vercel frontend

--- server.js
+++ server.js
@@ -22,7 +22,14 @@ connectDB().catch(err => {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000', 
+    'https://fooddelivery-project.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
